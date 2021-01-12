571538176741f70c242cf161f203a10cafe10688
Aashish Prasad <aashish.pd097@gmail.com>
2021-01-12 15:38:43 +0000

    app.js

--- Backend/tsv2json/app.js
+++ Backend/tsv2json/app.js
@@ -7,6 +7,7 @@ let call_num = 0;
 app.use(express.json());
 
 let parserTools = require('./tsv2json.js');
+const { isEmpty } = require("lodash");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,12 +25,20 @@ app.get('/api/parser/result', (req,res) => {
    filename = req.query.filename;
 
     // call getFileData
-    
-    let tsvData =  getFileData(filename);
-    
-    tsvData.then(function(fileData) {
-        res.send(fileData);
-     })
+    let tsvData = new Promise(function(resolve, reject) { 
+        getFileData(filename).then(function(fileData){
+            if(fileData != null){
+                resolve(res.send(fileData));
+            }else{
+                reject(console.log("Error:"+ filename + " file not found"))
+            }
+        })    
+    })
+
+    tsvData.then(function() {
+    }).catch(() =>{
+        res.send({error_result: "Filename: " + filename + " not found. Please re-check filename."})
+    })
 
 });
 
@@ -47,34 +56,42 @@ app.get('/api/parser/result/previous', (req,res) => {
 
 app.post('/api/parser', (req,res) => {
 
+    if(isEmpty(req.body)){
+        res.send({error_result: "Error: Please enter Valid Data."})
+    }else{
+        let tsvData = req.body.data.data;
+        if(tsvData == ""){
+            res.send({error_result: "Error: Please enter Valid Data."})
+        }else{
+            const fs = require('fs');
     
-    //let tsvData = req.body;
-    let tsvData = req.body.data.data;
-    const fs = require('fs');
-    
-    call_num = call_num + 1;
-    filename = "data" + call_num + ".txt";
+            call_num = call_num + 1;
+            filename = "data" + call_num + ".txt";
     
 
-    fs.writeFile("./tmp/" + filename, tsvData, function(err) {
-        if(err) {
-            return console.log(err);
+            fs.writeFile("./tmp/" + filename, tsvData, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            }); 
+
+            // call tsv2json Business Rules
+            let a = run(filename);
+                a.then(function(result) {
+                    res.send(result);
+            })
         }
-    }); 
-
-    // call tsv2json Business Rules
-    let a = run(filename);
-    a.then(function(result) {
-        res.send(result);
-     })
-
+    }
 });
 
 async function getFileData(filename){
     const fs = require('fs').promises;
-        return await fs.readFile("./tmp/" + filename, 'utf8', function(err, data) {
-            if (err) throw err;
-        });
+        try{     
+            return await fs.readFile("./tmp/" + filename, 'utf8');
+        }catch(err){
+            //console.log("Error in reading file. " + err);
+        };
+    return null;
 }
 
 async function run(filename)  {
